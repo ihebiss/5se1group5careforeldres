30b9d22e61c70c6361876f5119540377762af43e
yemen63194 <115785597+yemen63194@users.noreply.github.com>
2024-11-10 14:06:37 +0100

    Update WebConfig.java

--- src/main/java/com/example/carecareforeldres/Web/WebConfig.java
+++ src/main/java/com/example/carecareforeldres/Web/WebConfig.java
@@ -11,8 +11,7 @@ public class WebConfig  implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://192.168.177.128:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization");
     }
