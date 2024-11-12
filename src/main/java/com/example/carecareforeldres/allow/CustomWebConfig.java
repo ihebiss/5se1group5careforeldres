f44265d4871141eb05538538e269129054dc7c60
ihebiss <115627318+ihebiss@users.noreply.github.com>
2024-11-12 21:47:15 +0100

    Update CustomWebConfig.java

--- src/main/java/com/example/carecareforeldres/allow/CustomWebConfig.java
+++ src/main/java/com/example/carecareforeldres/allow/CustomWebConfig.java
@@ -9,7 +9,7 @@ public class CustomWebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
            registry.addMapping("/**")
-                .allowedOrigins("http://192.168.233.128:4200") // Ajoutez ici l'URL de votre application Angular
+                .allowedOrigins("http://192.168.13.130:4200") // Ajoutez ici l'URL de votre application Angular
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization");
