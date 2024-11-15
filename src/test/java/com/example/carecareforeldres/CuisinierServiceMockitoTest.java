93e05915e346364a3fbfe4c7db5f1c732b58686e
ltifimedamin <115627037+ltifimedamin@users.noreply.github.com>
2024-11-15 10:03:40 +0100

    Update CuisinierServiceMockitoTest.java

--- src/test/java/com/example/carecareforeldres/CuisinierServiceMockitoTest.java
+++ src/test/java/com/example/carecareforeldres/CuisinierServiceMockitoTest.java
@@ -37,18 +37,17 @@ public class CuisinierServiceMockitoTest {
     public void setUp() {
         MockitoAnnotations.openMocks(this);
 
-        // Créer des données factices pour les tests
         Cuisinier chef = new Cuisinier();
         chef.setIdC(1);
         chef.setScore(10);
         chef.setPlats(new ArrayList<>());
-        chef.setSalaire(1000.0f);  // Initialiser le salaire
+        chef.setSalaire(1000.0f);  
 
         Cuisinier autreCuisinier = new Cuisinier();
         autreCuisinier.setIdC(2);
         autreCuisinier.setScore(5);
         autreCuisinier.setPlats(new ArrayList<>());
-        autreCuisinier.setSalaire(800.0f);  // Initialiser le salaire
+        autreCuisinier.setSalaire(800.0f);  
 
         cuisiniers = Arrays.asList(chef, autreCuisinier);
 
@@ -66,10 +65,10 @@ public class CuisinierServiceMockitoTest {
 
     @Test
     public void testMettreAJourBadgesInteractions() throws Exception {
-        // Exécuter la méthode à tester
+       
         cuisinierService.mettreAJourBadges();
 
-        // Vérifier les interactions avec les repositories
+     
         verify(restaurantRepository, times(1)).findAll();
         verify(cuisinierRepository, atLeastOnce()).save(any(Cuisinier.class));
         verify(userRepository, atLeastOnce()).findById(anyInt());
